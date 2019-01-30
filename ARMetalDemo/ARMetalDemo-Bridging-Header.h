b21079650a40038aa95b8c82cbbcbfd9d3876a44
Elvis Nuñez <elvisnunez@me.com>
2019-01-30 13:00:30 +0100

    Initial commit

---
+++ ARMetalDemo/ARMetalDemo-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  ARMetalDemo-Bridging-Header.h
+//  ARMetalDemo
+//
+//  Created by Elvis Nuñez on 30/01/2019.
+//  Copyright © 2019 Elvis Nuñez. All rights reserved.
+//
+
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+
+#import "ShaderTypes.h"
